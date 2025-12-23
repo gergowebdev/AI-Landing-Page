382d3307e0a641bb10ea866b1a22c6fc6b16df6b
Gyenes Gergo <gergo.gyenes@gmail.com>
2025-12-23 20:10:27 +0100

    Starting the Hero Section, with a reusable component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <>
       <div className="pt-19 lg:pt-21 overflow-hidden">
         <Header />
+        <Hero />
       </div>
+
       <ButtonGradient />
     </>
   );
