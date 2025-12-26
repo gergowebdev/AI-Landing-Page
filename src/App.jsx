cfbfbd453029406e7c254090ec4afdb56c92f073
Gyenes Gergo <gergo.gyenes@gmail.com>
2025-12-26 22:05:07 +0100

    Finishing the Benefits component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
@@ -8,6 +9,7 @@ const App = () => {
       <div className="pt-19 lg:pt-21 overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
       </div>
 
       <ButtonGradient />
